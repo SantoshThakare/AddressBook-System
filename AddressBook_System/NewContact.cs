3019e4d1274238d3c7b03e2c4e08c7e6a3cb7565
SantoshThakare <santoshthakare50@gmail.com>
2022-02-28 19:25:57 +0530

    [santosh] Added Method Add Contact

--- AddressBook_System/NewContact.cs
+++ AddressBook_System/NewContact.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace AddressBook_System
 {
-    internal class NewContact
+    public class NewContact
     {
+        List<ContactManager> addressList = new List<ContactManager>();
+        public void AddContact(ContactManager contact)
+        {
+            addressList.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (var contact in addressList)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine("Last Name: " + contact.LastName);
+                Console.WriteLine("Address : " + contact.Address);
+                Console.WriteLine("City : " + contact.City);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Zip : " + contact.Zip);
+                Console.WriteLine("PhoneNumber : " + contact.PhoneNumber);
+                Console.WriteLine("Email : " + contact.Email);
+            }
+        }
+
     }
 }
+
--- AddressBook_System/Program.cs
+++ AddressBook_System/Program.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace AddressBook_System
 {
-    internal class Program
+     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+            static void Main(string[] args)
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("Welcome to AddressBook Program");
+                    Console.WriteLine(" Enter options : \n 1. Create Contacts" +
+                        "\n 2 Added Contacts ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+
+                        case 1:
+                            Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email");
+                            ContactManager newcontact = new ContactManager()
+                            {
+                                FirstName = Console.ReadLine(),
+                                LastName = Console.ReadLine(),
+                                Address = Console.ReadLine(),
+                                City = Console.ReadLine(),
+                                State = Console.ReadLine(),
+                                Zip = Console.ReadLine(),
+                                PhoneNumber = Console.ReadLine(),
+                                Email = Console.ReadLine(),
+                            };
+                            break;
+                    case 2:
+                        Console.WriteLine("Enter the Contact to be Add : FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        Console.WriteLine("Enter the Contact to be Add: ");
+                        ContactManager contact = new ContactManager();
+                        NewContact addressBook = new NewContact();
+                        addressBook.AddContact(contact);
+                        contact.FirstName = Console.ReadLine();
+                        contact.LastName = Console.ReadLine();
+                        contact.Address = Console.ReadLine();
+                        contact.City = Console.ReadLine();
+                        contact.State = Console.ReadLine();
+                        contact.Zip = Console.ReadLine();
+                        contact.PhoneNumber = Console.ReadLine();
+                        contact.Email = Console.ReadLine();
+                        addressBook.Display();
+                        break;
+                    case 3:
+                            flag = false;
+                            break;
+                    }
+                }
+            }
+        
     }
 }
